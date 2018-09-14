5768ac917490c792214e21076cb34decf582953f
Nils Kneemeyer <nils.kneemeyer@nordakademie.de>
2018-09-14 13:30:47 +0200

    init

---
+++ src/main/java/de/codemadness/strombergapi/StrombergApiApplication.java
@@ -0,0 +1,12 @@
+package de.codemadness.strombergapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrombergApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrombergApiApplication.class, args);
+	}
+}
